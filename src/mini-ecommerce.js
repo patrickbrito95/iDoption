d3a52565c944d797bc4cdde606b5be622d4f48af
Juliana Mattar <juliana.mattar@gmail.com>
2020-11-24 14:13:38 -0300

    teste

--- src/mini-ecommerce.js
+++ src/mini-ecommerce.js
@@ -1,36 +1,31 @@
-import React, { useState, UseState, Fragment } from 'react';
-import './mini-ecommerce.css';
-import Menu from './components/menu/Menu';
-import Produtos from './components/produtos/Produtos';
-import Checkout from './components/checkout/Checkout';
-import ListarProdutos from './components/produtos/Listar-Produtos';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Clima from './components/clima/Clima';
-import axios from 'axios';
-import Slider from './components/carousel/Slider';
-
-
+import React, { useState, UseState, Fragment } from "react";
+import "./mini-ecommerce.css";
+import Menu from "./components/menu/Menu";
+import Produtos from "./components/produtos/Produtos";
+import Checkout from "./components/checkout/Checkout";
+import ListarProdutos from "./components/produtos/Listar-Produtos";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Clima from "./components/clima/Clima";
+import axios from "axios";
+import Slider from "./components/carousel/Slider";
+import Formulario from "./components/formulario/formulario";
 
 function MiniEcommerce() {
-
-  const [carrinho, setCarrinho] = useState({produtos: [] });
+  const [carrinho, setCarrinho] = useState({ produtos: [] });
   const [exibirProdutos, setExibirProdutos] = useState(true);
   const [exibirCheckout, setExibirCheckout] = useState(false);
-  const [total, setTotal] = useState('0,00')
+  const [total, setTotal] = useState("0,00");
 
   return (
-   <div>
-     <Menu />
-     <Clima />
-     <Slider>
-
-     </Slider>
-     <Produtos />
-     <ListarProdutos />
-     <Checkout />
-  
-
-   </div>
+    <div>
+      <Menu />
+      <Clima />
+      <Slider></Slider>
+      <Produtos />
+      <ListarProdutos />
+      
+      <Formulario />
+    </div>
   );
 }
 
